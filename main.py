ec9de068b6621cb1fe9d9a58a4764444021b8b6a
JajmeLesLjcornes <gabgigon@gmail.com>
2025-09-24 10:59:46 +0200

    controle gauche droite

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+key_pressed = set()
+
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -22,15 +24,19 @@ while running:
     # =========================================================
     # 1.                Récupérer les événements
     # =========================================================
-
     for event in pygame.event.get():  # poll for events
 
         if event.type == pygame.QUIT:  # pygame.QUIT event => clic sur X pour fermer la fenêtre
             running = False
         print(event)
-        print(event.type)
         if event.type == pygame.KEYDOWN:
             print(event.__dict__["scancode"])
+            print(pygame.key.get_pressed()[event.__dict__["key"]])
+
+        if event.type == pygame.KEYDOWN:
+            key_pressed.add(event.__dict__["scancode"])
+        if event.type == pygame.KEYUP:
+            key_pressed.discard(event.__dict__["scancode"])
 
     # <Event(769-KeyUp {'unicode': 'q', 'key': 113, 'mod': 0, 'scancode': 4, 'window': None})>
     # <Event(768-KeyDown {'unicode': 'q', 'key': 113, 'mod': 0, 'scancode': 4, 'window': None})>
@@ -44,6 +50,12 @@ while running:
         # Mettre en pause le jeu
         # print("La fenêtre n'a plus le focus clavier !")
         pass
+    if key_pressed:
+        if (4 in key_pressed) ^ (7 in key_pressed):
+            if 4 in key_pressed:
+                player_pos.x -= 300 * dt
+            else:
+                player_pos.x += 300 * dt
 
     """Todo :
     Faire en sorte de tilter la plateforme pour orienter le rebond
